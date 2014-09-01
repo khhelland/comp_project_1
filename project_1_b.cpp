4e29b9cc1097e7a766900cc5ca10e2617810fd72
Knut Halvor Helland <knut.helland@gmail.com>
2014-09-01 15:36:38 +0200

    ser ut til aa funke, skriver ut maxforskjell mellom sim og ana

--- project_1_b.cpp
+++ project_1_b.cpp
@@ -11,9 +11,9 @@ int main(int argc, char* argv[]){
   
   vec u = zeros<vec>(n+2);
   vec f = zeros<vec>(n+2);
-  //vec ft = zeros<vec>(n);
   vec bv = zeros<vec>(n+2);
   
+  vec sol = zeros<vec>(n+2);
   
   double a = -1;
   double b = 2;
@@ -29,6 +29,7 @@ int main(int argc, char* argv[]){
     f(i) = 100*exp(-10*i*h);
   }
 
+  f = f*pow(h,2);
   
   for(int i=1; i<n;i++){
     bv(i+1) -= ac/bv(i);
@@ -38,10 +39,18 @@ int main(int argc, char* argv[]){
   u(n) = f(n)/bv(n-1);
   
   for(int i= (n-1); i>0 ; i--){
-    u(i) = (f(i) - c*u(i+1))/bv(i);
+    u(i) = (f(i) - a*u(i+1))/bv(i);
   }
   
+  
+  for(int i=0;i<(n+2);i++){
+    sol(i) = 1 - (1 - exp(-10))*i*h - exp(-10*i*h);
+  }
 
+
+  cout << max(abs(u-sol))<<endl;
+  
+  
   
   return 0;
 }

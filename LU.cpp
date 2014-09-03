f44804404b8ddcfe1b901339ade7c9e822045701
Knut Halvor Helland <knut.helland@gmail.com>
2014-09-03 16:15:29 +0200

    Har begynt på lu-programmet. Fjernet zeros fra hoved

--- LU.cpp
+++ LU.cpp
@@ -10,35 +10,37 @@ int main(int argc, char* argv[]){
   
   int n = atoi(argv[1]); 
   
-  vec u = zeros<vec>(n+2);
-  vec f(n+2);
-  vec bv(n+2);
+  mat A = zeros<mat>(n,n);
+  vec f(n);
+  vec u(n);
   
   double a = -1;
   double b = 2;
-  double ac = 1;
-  
+    
   double h = 1.0/(n+1);
   
-  bv.fill(b);
+  A(0,0) = b;
+  A(1,0) = a;
+  
+  for(int i=1;i<(n-1);i++){
+    A(i-1,i) = a;
+    A(i,i) = b;
+    A(i+1,i) = a;
+  }
 
+  A(n-2,n-1) = a;
+  A(n-1,n-1) = b;
     
-  for(int i=0; i<(n+2); i++){
+      
+    
+  for(int i=1; i<=n; i++){
     f(i) = 100*exp(-10*i*h);
   }
 
   f = f*pow(h,2);
   
-  for(int i=1; i<n; i++){
-    bv(i+1) -= ac/bv(i);
-    f(i+1)  -= a*f(i)/bv(i);
-  }
   
-  u(n) = f(n)/bv(n);
-  
-  for(int i= (n-1); i>0 ; i--){
-    u(i) = (f(i) - a*u(i+1))/bv(i);
-  }
+  //finn ut om lU-metoden
   
   u.save("data.dat",raw_ascii);
   
